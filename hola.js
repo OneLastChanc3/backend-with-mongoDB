3976f3ac5b3474c3c42ab4b42126a4c15aa2e161
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-03-23 17:17:06 -0500

    prueba

--- hola.js
+++ hola.js
@@ -1,1 +1,25 @@
-hola
+
+const http = require('http')
+
+const hostname = '127.0.0.1'
+const port = 3041
+
+const server = http.createServer((req, res) => {
+    if (req.url = "/") {
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text/plain')
+        res.end("hola webonazo")
+    } else if (req.url = "/login") {
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text/plain')
+        res.end("hola webin")
+    } else {
+        res.statusCode = 404
+        res.setHeader('Content-type', 'text/plain')
+        res.end("not found")
+    }
+})
+
+server.listen(port, hostname, () => {
+    console.log(`sever lisrten at https://${hostname}:${port}`)
+})
