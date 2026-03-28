e21b721c53c763b879a27e320f6012e837ce5fbe
OneLastChanc3 <ytcarlos8045@gmail.com>
2026-03-28 12:44:37 -0500

    crud

---
+++ expres/index.js
@@ -0,0 +1,56 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+
+let teaData = []
+let nextID = 1
+
+app.use(express.json())
+app.get('/', (req, res) => {
+    console.log("hola funciona")
+    res.send("Servidor funcionando 🚀")
+})
+app.post('/teas', (req, res) => {
+    const { name, price } = req.body
+    const newTea = { id: nextID++, name, price }
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+})
+
+app.get('/teas', (req, res) => {
+    res.status(200).send(teaData)
+})
+app.get('/teas/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+
+    if (!tea) {
+        res.status(404).send("tea not found ")
+    }
+    res.status(200).send(tea)
+})
+app.put('/teas/:id', (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+
+    if (!tea) {
+        res.status(404).send("tea not found ")
+    }
+    res.status(200).send(tea)
+    const { name, price } = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+})
+
+app.delete('/teas/:id', (req, res) => {
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+
+    if (index === -1) {
+        res.status(404).send("tea not found ")
+    }
+    teaData.splice(index, 1)
+    return res.status(200).send("deleted")
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
